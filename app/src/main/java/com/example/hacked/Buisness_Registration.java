7e4061696e9eeddcd1c652e65c8cbd8836c5a66c
DEBB2002 <debbiswas2002@gmail.com>
2022-06-26 01:52:39 +0530

    --third commit

--- app/src/main/java/com/example/hacked/Buisness_Registration.java
+++ app/src/main/java/com/example/hacked/Buisness_Registration.java
@@ -1,24 +1,26 @@
 package com.example.hacked;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 
-public class Buisness_Registration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    String[] buisness= { "Hotels/Restaurant", "Guide"};
-    String buis=" ";
+public class Buisness_Registration extends AppCompatActivity  {
     EditText name, email, password, confirmpassword;
     Button submit;
 
@@ -28,18 +30,21 @@ public class Buisness_Registration extends AppCompatActivity implements AdapterV
     FirebaseDatabase firebaseDatabase;
     FirebaseStorage firebaseStorage;
     CheckBox guidecheck;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_buisness_registration );
+        name = findViewById( R.id.Buisnessname);
+        email = findViewById( R.id.email );
+        password = findViewById( R.id.pasword );
+        progressDialog = new ProgressDialog( this );
+        progressDialog.setMessage( "Please Wait" );
+        progressDialog.setCancelable( false );
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        submit = findViewById( R.id.reg_btn );
 
-        Spinner s1=findViewById( R.id.spinner1 );
-        s1.setOnItemSelectedListener(this);
-
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,buisness);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        s1.setAdapter(aa);
         submit.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,29 +53,49 @@ public class Buisness_Registration extends AppCompatActivity implements AdapterV
                 String emailtext = email.getText().toString();
                 String passwordtext = password.getText().toString();
                 if (emailtext != "" && passwordtext != "" && nametext != "") {
-
+                    Register( nametext, emailtext, passwordtext );
                 }
             }
         } );
 
 
 
+    }
+    private void Register(String name, String email1, String password1) {
+        if (email1 != null && password1 != null) {
+            mAuth.createUserWithEmailAndPassword( email1, password1 ).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText( Buisness_Registration.this, "User Created Successfully", Toast.LENGTH_SHORT ).show();
+                        DatabaseReference databaseReference = firebaseDatabase.getReference().child( "Hotels" ).child( mAuth.getUid() );
+
+                        Users users = new Users( mAuth.getUid(), name, email1 );
+                        databaseReference.setValue( users ).addOnCompleteListener( new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
 
 
+                                    Toast.makeText( Buisness_Registration.this, "Hotel Created data stored", Toast.LENGTH_SHORT ).show();
+                                    // startActivity( new Intent(Register.this,Login.class) );
+
+
+                                } else {
+                                    progressDialog.dismiss();
+                                    Toast.makeText( Buisness_Registration.this, "Bello", Toast.LENGTH_SHORT ).show();
+                                }
+
+                            }
+                        } );
+                        DatabaseReference databaseReference1 = firebaseDatabase.getReference().child( "Guide" ).child( mAuth.getUid() );
+                        Users users1 = new Users( mAuth.getUid(), name, email1 );
+
+                    }
+                }
+            } );
+        }
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-buis=buisness[position];
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged( hasCapture );
-    }
 }
