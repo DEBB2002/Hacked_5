4e634ee187c2b91110480fb3179282dc5a214cb4
DEBB2002 <debbiswas2002@gmail.com>
2022-06-25 18:59:50 +0530

    --first commit

---
+++ app/src/main/java/com/example/hacked/User_Type.java
@@ -0,0 +1,14 @@
+package com.example.hacked;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Type extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_user_type );
+    }
+}
